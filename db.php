010f4a72e8cd1bc0139e6be69f49b2c1a38a04c6
key2-hue <keymaeda3782@gmail.com>
2020-03-15 20:20:40 +0900

    03/15 20:20

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 define('FILE_SIZE', 1 * 1024 * 1024);
 define('THUMB_WIDTH', 500);
-define('PHOTO_DIR' , __DIR__ . 'photos');
+define('PHOTO_DIR' , __DIR__ . '/photos');
 define('THMUB_DIR' , __DIR__ . '/thumbs');
 
 ini_set('display_errors', 1);
--- photo.php
+++ photo.php
@@ -3,9 +3,14 @@
 namespace MyPhoto;
 
 class Photo {
+  private $photoName;
+
   public function submit() {
     try {
       $this->checkImage();
+      $photo = $this->checkPhotoType();
+      $beforePhoto = $this->save($photo);
+      $this->createPhoto($beforePhoto);
     } catch(\Exception $e) {
       echo $e->getMessage();
       exit;
@@ -28,5 +33,41 @@ class Photo {
       default:
         throw new \Exception('Error' . $_FILES['image']['error']);
     }
+  }
+
+  private function checkPhotoType() {
+    $photoType = exif_imagetype($_FILES['image']['tmp_name']);
+    switch($photoType) {
+      case IMAGETYPE_GIF:
+        return 'gif';
+      case IMAGETYPE_JPEG:
+        return 'jpeg';
+      case IMAGETYPE_PNG:
+        return 'png';
+      default:
+        throw new \Exception('画像の種類が間違っています!');
+    }
+  }
+
+  private function save($photo) {
+    $this->photoName = sprintf(
+      '%s_%s.%s',
+      time(),
+      sha1(uniqid(mt_rand(), true)),
+      $photo
+    );
+    $path = PHOTO_DIR . '/' . $this->photoName;
+    $finalPath = move_uploaded_file($_FILES['image']['tmp_name'], $path);
+    if($finalPath === false) {
+      throw new \Exception('アップロードされていません');
+    }
+    return $path;
+  }
+
+  private function createPhoto($beforePhoto) {
+    $photoSize = getimagesize($beforePhoto);
+    $width = $photoSize[0];
+    $height = $photoSize[1];
+    if 
   }
 }
